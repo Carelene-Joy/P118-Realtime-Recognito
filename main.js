0ae30e27bde08092ab5c763d2c3705fe46e5b2c6
Carelene-Joy <74822598+Carelene-Joy@users.noreply.github.com>
2021-10-20 14:33:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function setup () {
+    var canvas = createCanvas(300,300)
+    canvas.position(540,320)
+  video = createCapture(VIDEO)
+    video.hide()
+    
+} 
+
+function draw () {
+  // we will need this function for the next continuation so just keep it open
+}
+
+/* use span or div tags like this to write the names of your family members = 
+<div><p class="btn btn-primary">Father | Mother | Sister | Myself</p></div>*/
+
+/* use bootstrap elements like btn btn-color and 
+box shadow in style.css and index.html to make the webpage look good*/
+
+/* Basically in this project we only have design the canvas so try to do your best*/
+
+// We are done for now!
